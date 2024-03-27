7a122283c42fa14db677611621d29220a84011ce
odevbruno <bruno.oliveira@externos-br.cencosud.com>
2024-03-26 21:37:09 -0300

    first commit

---
+++ tests/SampleERC20_test.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.5.9;
+import "remix_tests.sol";
+import "../contracts/SampleERC20.sol";
+
+contract SampleERC20Test {
+
+    SampleERC20 s;
+    function beforeAll () public {
+        s = new SampleERC20("BOToken", "BOT", 18, 1000);
+    }
+
+    function testTokenNameAndSymbol () public {
+        Assert.equal(s.name(), "BOToken", "token name did not match");
+        Assert.equal(s.symbol(), "BOT", "token symbol did not match");
+    }
+}
